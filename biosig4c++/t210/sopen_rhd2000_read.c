a2e729c96ae34d877f91f2ad6c65844b544db77f
Alois Schloegl <alois.schloegl@ist.ac.at>
2020-01-16 22:49:26 +0100

    [b4c] support for RHS2000 format added

--- biosig4c++/t210/sopen_rhd2000_read.c
+++ biosig4c++/t210/sopen_rhd2000_read.c
@@ -1,6 +1,5 @@
 /*
-
-    Copyright (C) 2019 Alois Schloegl <alois.schloegl@gmail.com>
+    Copyright (C) 2019,2020 Alois Schloegl <alois.schloegl@gmail.com>
 
     This file is part of the "BioSig for C/C++" repository
     (biosig4c++) at http://biosig.sf.net/
@@ -9,16 +8,15 @@
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 3
     of the License, or (at your option) any later version.
-
  */
 
 /*
-
-Referemces:
+References:
 [1]  RHD2000 Application Note: Data File Formats, Intan TECHNOLOGIES, LLC
-     Downloaded 2019-08-30 from
-     https://www.intantech.com/files/Intan_RHD2000_data_file_formats.pdf
-
+     Downloaded 2020-01-14 from
+     http://www.intantech.com/files/Intan_RHD2000_data_file_formats.pdf
+[2]  RHS2000 Data File Formats - Intan Tech
+     http://www.intantech.com/files/Intan_RHS2000_data_file_formats.pdf
 */
 
 #include <ctype.h>
@@ -26,6 +24,7 @@ Referemces:
 #include <string.h>
 #include <iconv.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #if !defined(__APPLE__) && defined (_LIBICONV_H)
  #define iconv		libiconv
@@ -41,7 +40,7 @@ Referemces:
 
 typedef struct {
 	int32_t length;	// this is little endian as defined in spec, use le32toh() when using it
-	void *string;
+	char *string;
 } QSTRING_T;
 
 #ifdef __cplusplus
@@ -52,9 +51,9 @@ extern "C" {
    reads more data if needed, advance position pointer pos to the end of the string;
 */
 QSTRING_T *read_qstring(HDRTYPE* hdr, size_t *pos) {
-	int32_t len0 = lei32p(hdr->AS.Header + (*pos));
+	uint32_t len = leu32p(hdr->AS.Header + (*pos));
 	*pos += 4;
-	int32_t len = max(0,len0);
+	if (len==(uint32_t)(-1)) return NULL;
 
 	// after each qstring at most 28 bytes are loaded before the next check for a qstring
 	// This check is also needed when qstring is empty
@@ -70,14 +69,22 @@ QSTRING_T *read_qstring(HDRTYPE* hdr, size_t *pos) {
 		hdr->HeadLen += ifread(hdr->AS.Header+hdr->HeadLen, 1, SIZE-hdr->HeadLen, hdr);
 	}
 
-	if (len0 < 0) return NULL;
-
-	if ((*pos + len) > hdr->HeadLen)
+	if ((*pos + len ) > hdr->HeadLen)
 		biosigERROR(hdr, B4C_INCOMPLETE_FILE, "Format Intan RHD2000 - incomplete file");
 
-	QSTRING_T *qString=(QSTRING_T *)(hdr->AS.Header+(*pos));
+	QSTRING_T *qString=(QSTRING_T *)(hdr->AS.Header+(*pos)-4);
 	*pos += len;
 	return qString;
+}
+
+size_t  convert_qstring_to_utf8(QSTRING_T *S, char **__restrict outbuf, size_t *__restrict outlen) {
+	if (S==NULL) return 0;
+	if (S->length==(-1)) return 0;
+	iconv_t CD = iconv_open ("UTF-8", "UTF-16LE");
+	size_t inlen = le32toh(S->length);
+	char *inbuf = &(S->string);
+	size_t iconv_res = iconv (CD, &inbuf, &inlen, outbuf, outlen);
+	iconv_close (CD);
 }
 
 
@@ -239,21 +246,200 @@ int sopen_intan_clp_read(HDRTYPE* hdr) {
 }
 
 int sopen_rhs2000_read(HDRTYPE* hdr) {
-	biosigERROR(hdr, B4C_FORMAT_UNSUPPORTED, "Format Intan/RHS2000 not supported");
-	return -1;
+		int gdftyp = 4;
+		char bufstr[1000];
+		size_t outlen=1000;
+		char *tmpstr=bufstr;
+
+		size_t c= convert_qstring_to_utf8((QSTRING_T *)(hdr->AS.Header+0x64), &tmpstr, &outlen);
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) [%u] <%s> %d\n",__FILE__,__LINE__,__func__,hdr->HeadLen,bufstr,outlen);
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) [%u]\n",__FILE__,__LINE__,__func__,hdr->HeadLen);
+
+		// 8 bytes
+		float minor = leu16p(hdr->AS.Header+6);
+		minor      *= (minor < 10) ? 0.1 : 0.01;
+		hdr->VERSION = leu16p(hdr->AS.Header+4) + minor;
+
+		// +38 = 46 bytes
+		hdr->NS = 1;
+		hdr->SampleRate = lef32p(hdr->AS.Header+8);
+
+		float HighPass = ( leu16p(hdr->AS.Header+12) ? lef32p(hdr->AS.Header+14) : 0.0 );
+		      HighPass = max( HighPass, lef32p(hdr->AS.Header+18) );
+		float LowPass = lef32p(hdr->AS.Header+26);
+
+		// +10 = 56 bytes
+		const int ListNotch[] = {0,50,60};
+		uint16_t tmp = leu16p(hdr->AS.Header+46);
+		if (tmp>2) tmp=0;
+		float Notch = ListNotch[tmp];
+		float fZ_desired   = lef32p(hdr->AS.Header+46); // desired impedance test frequency
+		float fZ_actual    = lef32p(hdr->AS.Header+50); // actual impedance test frequency
+
+		// +4 = 60 bytes
+		// +12 = 72 bytes
+
+		size_t pos = 72;
+		QSTRING_T *note1 = read_qstring(hdr, &pos);
+		QSTRING_T *note2 = read_qstring(hdr, &pos);
+		QSTRING_T *note3 = read_qstring(hdr, &pos);
+
+		uint16_t flag_DC_amplifier_data_saved = leu16p(hdr->AS.Header+pos);
+		pos += 2;
+		uint16_t flag_Board_Mode = leu16p(hdr->AS.Header+pos);
+		pos += 2;
+
+		QSTRING_T *ReferenceChannelName = read_qstring(hdr, &pos);
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...)  %d %d\n", __FILE__, __LINE__, __func__, pos, hdr->HeadLen);
+
+		uint16_t numberOfSignalGroups = leu16p(hdr->AS.Header+pos);
+		pos += 2;
+		uint16_t NS = 0;
+		// read all signal groups
+		for (int nsg=0; nsg<numberOfSignalGroups; nsg++) {
+			QSTRING_T *SignalGroupName = read_qstring(hdr, &pos);
+			QSTRING_T *SignalGroupPrefix = read_qstring(hdr, &pos);
+			uint16_t flag_SignalGroupEnabled = leu16p(hdr->AS.Header+pos);
+			pos += 2;
+			uint16_t NumberOfChannelsInSignalGroup = leu16p(hdr->AS.Header+pos);
+			pos += 2;
+			uint16_t NumberOfAmplifierChannelsInSignalGroup = leu16p(hdr->AS.Header+pos);
+			pos += 2;
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) group=%u %u+%u %d\n", __FILE__, __LINE__, __func__, nsg, NS, NumberOfChannelsInSignalGroup, pos);
+
+		//if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) group=%u %s %s\n",__FILE__,__LINE__,__func__,nsg,SignalGroupName->string,SignalGroupPrefix->string);
+
+			if (flag_SignalGroupEnabled) {
+				hdr->CHANNEL = (CHANNEL_TYPE*) realloc(hdr->CHANNEL, (1+NS+NumberOfChannelsInSignalGroup) * sizeof(CHANNEL_TYPE));
+				for (unsigned k = 0; k < NumberOfChannelsInSignalGroup; k++) {
+					QSTRING_T *NativeChannelName = read_qstring(hdr, &pos);
+					QSTRING_T *customChannelName = read_qstring(hdr, &pos);
+					pos += 4;
+					int16_t SignalType = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t ChannelEnabled = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t ChipChannel = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t CommandStream = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t BoardStream = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t SpikeScopeVoltageTriggerMode = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t SpikeScopeVoltageThreshold = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t SpikeScopeDigitalTriggerChannel = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					int16_t SpikeScopeDigitalTriggerEdgePolarity = lei16p(hdr->AS.Header+pos);
+					pos += 2;
+					float ElectrodeImpedanceMagnitude = lef32p(hdr->AS.Header+pos);
+					pos += 4;
+					float ElectrodeImpedancePhase = lef32p(hdr->AS.Header+pos);
+					pos += 4;
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) group=%u %u %d %d\n",__FILE__,__LINE__,__func__,nsg,k,ChannelEnabled,pos);
+
+					if (ChannelEnabled) {
+						NS++;	// first channel is Time channel
+						CHANNEL_TYPE *hc = hdr->CHANNEL + NS;
+
+//		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) %u %2u %3u [%c %c]\n",__FILE__,__LINE__,__func__,nsg,k,NS,((char*)customChannelName->string)[0],((char*)customChannelName->string)[1]);
+						size_t outlen = MAX_LENGTH_LABEL+1;
+						char *outbuf = hc->Label;
+						size_t c = convert_qstring_to_utf8(NativeChannelName, &outbuf, &outlen);
+
+#ifdef MAX_LENGTH_PHYSDIM
+						strcpy(hc->PhysDim,"?");
+#endif
+						hc->OnOff  = 1;
+						hc->SPR    = hdr->SPR;
+						hc->GDFTYP = 4; // uint16
+						hc->bi     = 4-2+NS*2;
+						hc->bi8    = hc->bi << 3;
+						hc->LeadIdCode = 0;
+						hc->DigMin = 0.0;
+						hc->DigMax = ldexp(2,16)-1;
+						hc->Off    = 0.0;
+						hc->Cal    = 1.0 / hdr->SampleRate;
+						hc->PhysDimCode = 0; // [?]
+
+						hc->PhysMin = -1.0;
+						hc->PhysMax = +1.0;
+
+						hc->bufptr = NULL;
+						hc->TOffset = 0;
+						hc->LowPass = LowPass;
+						hc->HighPass = HighPass;
+						hc->Notch = Notch;
+						hc->XYZ[0] = NAN;
+						hc->XYZ[1] = NAN;
+						hc->XYZ[2] = NAN;
+						hc->Impedance = ElectrodeImpedanceMagnitude;
+
+		if (VERBOSE_LEVEL>7) fprintf(stdout,"%s (line %u); %s(...) %u %2u %3u Label<%s>: %d\n",__FILE__,__LINE__,__func__,nsg,k,NS,hc->Label,c);
+
+					}
+				}
+			}
+		}
+		{	// channel 0 - time channel
+						CHANNEL_TYPE *hc = hdr->CHANNEL+0;
+						hc->OnOff = 2;
+						strcpy(hc->Label, "time");
+						strcpy(hc->Transducer, "");
+#ifdef MAX_LENGTH_PHYSDIM
+						strcpy(hc->PhysDim,"s");
+#endif
+						hc->SPR = hdr->SPR;
+						hc->GDFTYP  = 6; // uint32
+						hc->bi      = 0;
+						hc->bufptr  = NULL;
+
+						hc->OnOff  = 1;
+						hc->SPR    = hdr->SPR;
+						hc->GDFTYP = 4; // uint16
+						hc->bi     = 4-2+NS*2;
+						hc->bi8    = hc->bi << 3;
+						hc->LeadIdCode = 0;
+						hc->DigMin = 0.0;
+						hc->DigMax = ldexp(2,32)-1;
+						hc->Off    = 0.0;
+						hc->Cal    = 1.0/hdr->SampleRate;
+						hc->PhysDimCode = 2176; // [s]
+						hc->PhysMin = 0;
+						hc->PhysMax = hc->DigMax*hc->Cal;
+
+						hc->bufptr = NULL;
+						hc->TOffset = 0;
+						hc->LowPass = 0;
+						hc->HighPass = INFINITY;
+						hc->Notch = 0;
+						hc->XYZ[0] = NAN;
+						hc->XYZ[1] = NAN;
+						hc->XYZ[2] = NAN;
+						hc->Impedance = NAN;
+		}
+
+		hdr->HeadLen = pos;
+		hdr->SPR = 128;
+		hdr->NRec = -1;
+		hdr->NS = NS;
+		hdr->AS.bpb = (4+2*NS)*hdr->SPR;
+		ifseek(hdr, hdr->HeadLen, SEEK_SET);
+
+		struct stat FileBuf;
+		if (stat(hdr->FileName,&FileBuf)==0) hdr->FILE.size = FileBuf.st_size;
+		hdr->NRec = (hdr->FILE.size - hdr->HeadLen) / hdr->AS.bpb;
+
+		return 0;
 }
 
 int sopen_rhd2000_read(HDRTYPE* hdr) {
-/*
-	this function is a stub or placeholder and need to be defined in order to be useful.
-	It will be called by the function SOPEN in "biosig.c"
-
-	Input:
-		char* Header	// contains the file content
-
-	Output:
-		HDRTYPE *hdr	// defines the HDR structure accoring to "biosig.h"
-*/
 
 		float minor = leu16p(hdr->AS.Header+6);
 		minor      *= (minor < 10) ? 0.1 : 0.01;
@@ -262,9 +448,10 @@ int sopen_rhd2000_read(HDRTYPE* hdr) {
 		hdr->NS = 1;
 		hdr->SampleRate = lef32p(hdr->AS.Header+8);
 
-		float HighPass = ( leu16p(hdr->AS.Header+12) ? 0.0 : lef32p(hdr->AS.Header+14) );
+		float HighPass = ( leu16p(hdr->AS.Header+12) ? lef32p(hdr->AS.Header+14) : 0.0 );
 		      HighPass = max( HighPass, lef32p(hdr->AS.Header+18) );
 		float LowPass = lef32p(hdr->AS.Header+22);
+
 		const int ListNotch[] = {0,50,60};
 		uint16_t tmp = leu16p(hdr->AS.Header+34);
 		if (tmp>2) tmp=0;
@@ -329,27 +516,15 @@ int sopen_rhd2000_read(HDRTYPE* hdr) {
 				for (; chan < hdr->NS; chan++); {
 					CHANNEL_TYPE *hc = hdr->CHANNEL + chan;
 
-					QSTRING_T *nativeChannelName = read_qstring(hdr, &pos);
-					if (nativeChannelName != NULL) {
-						// translate into biosig HDR channel structure
-						iconv_t CD = iconv_open ("UTF-16LE","UTF-8");
-						size_t inlen = le32toh(nativeChannelName->length);
-						size_t outlen = MAX_LENGTH_LABEL+1;
-						char *outbuf = hc->Label;
-						size_t iconv_res = iconv (CD, nativeChannelName->string, &inlen, &outbuf, &outlen);
-						iconv_close (CD);
-					}
+					QSTRING_T *NativeChannelName = read_qstring(hdr, &pos);
+					size_t outlen = MAX_LENGTH_LABEL+1;
+					char *outbuf = hc->Label;
+					size_t c = convert_qstring_to_utf8(NativeChannelName, &outbuf, &outlen);
 
-					QSTRING_T *customChannelName = read_qstring(hdr, &pos);
-					if (customChannelName != NULL) {
-						// translate into biosig HDR channel structure
-						iconv_t CD = iconv_open ("UTF-16LE","UTF-8");
-						size_t inlen = le32toh(customChannelName->length);
-						size_t outlen = MAX_LENGTH_LABEL+1;
-						char *outbuf = hc->Label;
-						size_t iconv_res = iconv (CD, customChannelName->string, &inlen, &outbuf, &outlen);
-						iconv_close (CD);
-					}
+					QSTRING_T *CustomChannelName = read_qstring(hdr, &pos);
+					outlen = MAX_LENGTH_LABEL+1;
+					outbuf = hc->Label;
+					c = convert_qstring_to_utf8(CustomChannelName, &outbuf, &outlen);
 
 					uint16_t customOrder = leu16p(hdr->AS.Header+pos);
 					pos += 2;
@@ -433,8 +608,6 @@ int sopen_rhd2000_read(HDRTYPE* hdr) {
 	return -1;
 }
 
-
 #ifdef __cplusplus
 }
 #endif
-
