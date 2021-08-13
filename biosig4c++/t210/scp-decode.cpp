0cb889c84d39cd11f197fa8e943489d827ac5b7b
Alois SCHLOEGL <alois.schloegl@ist.ac.at>
2021-08-13 10:39:34 +0200

    changes required for build on MacOSX

--- biosig4c++/t210/scp-decode.cpp
+++ biosig4c++/t210/scp-decode.cpp
@@ -102,6 +102,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //#include <iostream>
 #include <cstring>    //strcat, strcpy
 #include <cstdio>
+#include <stdlib.h>
 using namespace std;
 
 /*
